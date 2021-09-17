9c6c25e98c4031d7a718447463da9706ffbdf70e
SANJAY GAUTAM <SANJAY GAUTAM@sanjaykumarsingh9419>
2021-09-17 14:31:33 +0530

    WH-12-again updated

--- src/main/java/com/sts/TestService.java
+++ src/main/java/com/sts/TestService.java
@@ -1,5 +1,13 @@
 package com.sts;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TestService {
+	
+	@GetMapping("/hello")
+	public String getService()
+	{
+		return "hello ";
+	}
 
 }
